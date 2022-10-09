4aee9733f562f14cc4c02d4e9ba1865cab281e2a
student <student@akademiakaliska.edu.pl>
2022-10-09 10:52:26 +0200

    Dodano walidację metody set

--- Category.java
+++ Category.java
@@ -4,8 +4,13 @@ public class Category {
     public String getName(){
         return name;
     }
+
     public void setName(String name){
-        this.name=name;
+        if(!name.isEmpty() || !name.lenght>0){
+            this.name=name;
+        }else{
+            System.out.println("Podano niepoprawna nazwe");
+        }
     }
 
     public String toString() {
