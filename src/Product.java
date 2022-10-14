69970ef95cf58b8f89a4426d81446c7c1234c4dc
Patrycja <patrycja.wanga@gmail.com>
2022-10-14 19:17:18 +0200

    Utworzono klase Product

---
+++ src/Product.java
@@ -0,0 +1,34 @@
+public class Product {
+    private String name = null;
+    private Category category = null;
+    private float price = 0;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Produkt: " + name + category + "Cena: " + price;
+    }
+}
